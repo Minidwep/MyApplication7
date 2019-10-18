a6647ddccb7d64d161490e99d67f662489d75767
Minidwep <943490180@qq.com>
2019-10-18 16:09:40 +0800

    Initial commit

---
+++ app/src/main/java/com/minidwep/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.minidwep.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    } protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
