b84b62f1955205eb5e078fa63ecc2bf791a3616c
Minidwep <943490180@qq.com>
2019-10-18 16:32:40 +0800

    this is my test

---
+++ app/src/main/java/com/minidwep/myapplication/MainActivity12.java
@@ -0,0 +1,30 @@
+package com.minidwep.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    } protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        111
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
